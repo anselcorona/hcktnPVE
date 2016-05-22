ea8a3e131ccfa2650415715a2fcce75c93126851
Gleyder Hernandez <gleyderwebs@gmail.com>
2016-05-21 16:34:15 -1200

    sss

---
+++ PVE/Plataforma/AddComment.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page MasterPageFile="Principal.master" Language="C#" AutoEventWireup="true" CodeBehind="AddComment.aspx.cs" Inherits="Plataforma.AddComment" %>
+<asp:Content ContentPlaceHolderId="Body" runat="server">
+    <h3>
+        
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        
+    </h3>
+    Agregar un nuevo comentario:<br />
+    <br />
+    Tipo de Comentario.<br />
+    <asp:DropDownList ID="DropDownList1" runat="server" Height="62px" Width="211px">
+        <asp:ListItem>Aporte</asp:ListItem>
+        <asp:ListItem>Contraargumento</asp:ListItem>
+    </asp:DropDownList>
+    <br />
+    <br />
+    Contenido:<br />
+    <asp:TextBox ID="TextBox1" runat="server" Height="209px" TextMode="MultiLine" Width="580px"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Button1" runat="server" Height="45px" OnClick="Button1_Click" Text="Agregar Comentario" Width="227px" />
+    <br />
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Comentarios]"></asp:SqlDataSource>
+    </asp:Content>
---
+++ PVE/Plataforma/AddComment.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Plataforma
+{
+    public partial class AddComment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string user = Session["User"].ToString();
+            }
+            catch
+            {
+                Server.Transfer("Login.aspx");
+            }
+
+
+            Label1.Text = GetValue("Descripcion");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AgregarComentario(string tipo, string coment, string codigo)
+        {
+            SqlConnection con = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
+
+            SqlCommand cmd = new SqlCommand("Insert into Comentarios(Codigo,Autor,Contenido,IdeaImplicada,Tipo) values(" + codigo + ",'" + Session["User"].ToString() + "','" + coment + "','" + Session["Idea"] + "','" + tipo + "')", con);
+
+                try
+
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+
+
+                    con.Close();
+
+                    //Se agrego Correctamente!
+                    Server.Transfer("Login.aspx", true);
+                }
+
+                catch
+
+                {
+
+                    
+                    Label1.Text = "Datos Invalidos Agregados, Verifique Por Favor";
+
+                }
+            }
+
+
+
+
+        private int ObtenerCantidadComentarios()
+        {
+
+            SqlConnection con = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
+            con.Open();
+
+            string strSql = "SELECT COUNT(Codigo) FROM Comentarios";
+            SqlCommand command = new SqlCommand(strSql, con);
+            int count = Convert.ToInt32(command.ExecuteScalar());
+            con.Close();
+
+            return count;
+        }
+
+
+
+        private string GetValue(string columna) {
+            string n = null;
+            SqlConnection con = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM IdeasEmprendedoras where Codigo = '" + Session["Idea"].ToString()  + "'", con);
+            con.Open();
+
+            System.Data.IDataReader dataReader = cmd.ExecuteReader();
+            {
+                //Loop through your results
+                while (dataReader.Read())
+                {
+                    n = (Convert.ToString(dataReader[columna]));
+                }
+
+               
+            }
+            return n;
+        }
+    }
+}
